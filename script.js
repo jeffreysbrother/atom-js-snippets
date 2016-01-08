13e4ad6123828356c010078564b67276c1d05f32
James Cool <jeffreysbrother@gmail.com>
2016-01-07 16:16:33 -0800

    added first batch of newly-installed JS snippets

---
+++ script.js
@@ -0,0 +1,39 @@
+[fn]
+function methodName (arguments) {
+  // body...
+}
+
+
+[afn]
+function(arguments) {
+  // body...
+}
+
+
+[iife]
+(function() {
+  'use strict';
+
+}());
+
+
+[ae]
+document.addEventListener('event', function(e) {
+  // body...
+});
+
+
+[gi]
+document.getElementById('id');
+
+
+[gc]
+document.getElementsByClassName('class');
+
+
+[gt]
+document.getElementsByTagName('tag');
+
+
+[us]
+'use strict';
